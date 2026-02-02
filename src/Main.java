af02a03b44b2eea7d3bfffc571c182039a20d270
KayleeChan <kayleec55@nycstudents.net>
2026-02-02 12:08:37 -0500

    Added test cases in the Main class.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main
 {
-
+    public static void main(String[] args)
+    {
+        MessageBuilder m = new MessageBuilder("the");
+        System.out.println(m.getAbbreviation());
+        MessageBuilder b = new MessageBuilder("good");
+        System.out.println(b.getAbbreviation());
+    }
 }
