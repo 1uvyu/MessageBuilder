69c37c7fe7836faf26c990b735af06979619a533
KayleeChan <kayleec55@nycstudents.net>
2026-02-02 12:26:09 -0500

    Debugged the MessageBuilder constructor and getAbbreviation method.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        MessageBuilder m = new MessageBuilder("the");
+        MessageBuilder m = new MessageBuilder("The");
         System.out.println(m.getAbbreviation());
         MessageBuilder b = new MessageBuilder("good");
         System.out.println(b.getAbbreviation());
--- src/MessageBuilder.java
+++ src/MessageBuilder.java
@@ -7,14 +7,15 @@ public class MessageBuilder
     {
         String currentWord = startingWord;
         numWords = 1;
-        message = startingWord + " ";
-        while (!(nextWord == null))
+        message = startingWord;
+        while (!(getNextWord(currentWord) == null))
         {
-            message += getNextWord(currentWord) + " ";
+            message += " " + getNextWord(currentWord);
             currentWord = getNextWord(currentWord);
             numWords++;
         }
     }
+
     /**
      * Returns a word to follow the word specified by the
      * parameter or null if there are no remaining words.
@@ -22,7 +23,17 @@ public class MessageBuilder
      * Postcondition: Returns an individual word with no spaces.
      */
     public String getNextWord(String s)
-    { /* implementation not shown */ }
+    {
+        String[] tests = {"The", "book", "on", "the", "table", null, "good", "morning", "sunshine", null, "a", "b", "c", "d", "e", "f", "g", "h", null, "nice", "job", null, "yikes", null, "As", "soon", "as", "possible", null};
+        for (int i = 0; i < tests.length; i++) {
+            if (tests[i] != null) {
+                if (tests[i].equals(s)) {
+                    return tests[i + 1];
+                }
+            }
+        }
+        return null;
+    }
     /**
      * Returns an abbreviation for the instance variable message,
      * as described in part (b)
@@ -32,7 +43,7 @@ public class MessageBuilder
     public String getAbbreviation()
     {
         String abbr = message.substring(0, 1);
-        for (int i = 0; i <= message.length(); i++)
+        for (int i = 0; i < message.length(); i++)
         {
             if (message.substring(i , i + 1).equals(" ")) abbr += message.substring(i + 1, i + 2);
         }
